3be5f073e35f60f002d611b8e36f4fd04e18c4ac
Crowdbotics <team@crowdbotics.com>
2020-07-13 20:59:08 +0000

    Update store

--- src/store/services.js
+++ src/store/services.js
@@ -3,6 +3,10 @@ import {
   ALBUM_AND_TRACKS_USERNAME,
   ALBUM_AND_TRACKS_PASSWORD
 } from "react-native-dotenv"
+const publicplugin = axios.create({
+  baseURL: "https://api.spotify.com/v1",
+  headers: { Accept: "application/json", "Content-Type": "application/json" }
+})
 const albumandTracks = axios.create({
   baseURL: "https://api.spotify.com/v1",
   auth: {
@@ -11,10 +15,9 @@ const albumandTracks = axios.create({
   },
   headers: { Accept: "application/json", "Content-Type": "application/json" }
 })
-const testSharing11API = axios.create({
-  baseURL: "https://testsharing11-dev-7520.herokuapp.com/",
-  headers: { Accept: "application/json", "Content-Type": "application/json" }
-})
+function get_albums_read({ market, ids }) {
+  return publicplugin.get(`/albums`, { params: { market: market, ids: ids } })
+}
 function get_albums_list({ ids, market }) {
   return albumandTracks.get(`/albums`, { params: { ids: ids, market: market } })
 }
@@ -26,98 +29,9 @@ function get_albums_id_tracks_list(id, { limit, offset, market, flag }) {
     params: { limit: limit, offset: offset, market: market, flag: flag }
   })
 }
-function api_v1_customtext_list() {
-  return testSharing11API.get(`/api/v1/customtext/`)
-}
-function api_v1_customtext_read() {
-  return testSharing11API.get(`/api/v1/customtext/{id}/`)
-}
-function api_v1_customtext_update(requestBody) {
-  return testSharing11API.put(`/api/v1/customtext/{id}/`, requestBody)
-}
-function api_v1_customtext_partial_update(requestBody) {
-  return testSharing11API.patch(`/api/v1/customtext/{id}/`, requestBody)
-}
-function api_v1_homepage_list() {
-  return testSharing11API.get(`/api/v1/homepage/`)
-}
-function api_v1_homepage_read() {
-  return testSharing11API.get(`/api/v1/homepage/{id}/`)
-}
-function api_v1_homepage_update(requestBody) {
-  return testSharing11API.put(`/api/v1/homepage/{id}/`, requestBody)
-}
-function api_v1_homepage_partial_update(requestBody) {
-  return testSharing11API.patch(`/api/v1/homepage/{id}/`, requestBody)
-}
-function api_v1_login_create() {
-  return testSharing11API.post(`/api/v1/login/`)
-}
-function api_v1_signup_create(requestBody) {
-  return testSharing11API.post(`/api/v1/signup/`, requestBody)
-}
-function rest_auth_login_create(requestBody) {
-  return testSharing11API.post(`/rest-auth/login/`, requestBody)
-}
-function rest_auth_logout_list() {
-  return testSharing11API.get(`/rest-auth/logout/`)
-}
-function rest_auth_logout_create() {
-  return testSharing11API.post(`/rest-auth/logout/`)
-}
-function rest_auth_password_change_create(requestBody) {
-  return testSharing11API.post(`/rest-auth/password/change/`, requestBody)
-}
-function rest_auth_password_reset_create(requestBody) {
-  return testSharing11API.post(`/rest-auth/password/reset/`, requestBody)
-}
-function rest_auth_password_reset_confirm_create(requestBody) {
-  return testSharing11API.post(
-    `/rest-auth/password/reset/confirm/`,
-    requestBody
-  )
-}
-function rest_auth_registration_create(requestBody) {
-  return testSharing11API.post(`/rest-auth/registration/`, requestBody)
-}
-function rest_auth_registration_verify_email_create(requestBody) {
-  return testSharing11API.post(
-    `/rest-auth/registration/verify-email/`,
-    requestBody
-  )
-}
-function rest_auth_user_read() {
-  return testSharing11API.get(`/rest-auth/user/`)
-}
-function rest_auth_user_update(requestBody) {
-  return testSharing11API.put(`/rest-auth/user/`, requestBody)
-}
-function rest_auth_user_partial_update(requestBody) {
-  return testSharing11API.patch(`/rest-auth/user/`, requestBody)
-}
 export const apiService = {
+  get_albums_read,
   get_albums_list,
   get_albums_id_read,
-  get_albums_id_tracks_list,
-  api_v1_customtext_list,
-  api_v1_customtext_read,
-  api_v1_customtext_update,
-  api_v1_customtext_partial_update,
-  api_v1_homepage_list,
-  api_v1_homepage_read,
-  api_v1_homepage_update,
-  api_v1_homepage_partial_update,
-  api_v1_login_create,
-  api_v1_signup_create,
-  rest_auth_login_create,
-  rest_auth_logout_list,
-  rest_auth_logout_create,
-  rest_auth_password_change_create,
-  rest_auth_password_reset_create,
-  rest_auth_password_reset_confirm_create,
-  rest_auth_registration_create,
-  rest_auth_registration_verify_email_create,
-  rest_auth_user_read,
-  rest_auth_user_update,
-  rest_auth_user_partial_update
+  get_albums_id_tracks_list
 }
